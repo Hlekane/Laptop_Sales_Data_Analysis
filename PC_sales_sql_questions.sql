def9554b8a0cfa47a07d984b517d06ad298ff0b4
Hlekane Ngobeni <120193674+Hlekane@users.noreply.github.com>
2026-03-09 19:24:34 +0200

    Add SQL queries for sales analysis

--- PC_sales_sql_questions.sql
+++ PC_sales_sql_questions.sql
@@ -133,6 +133,69 @@ SELECT Payment_Method,AVG(Credit_Score) AS Average_Score_Payment_Method
 	GROUP BY Payment_Method
 
 -- 20. Calculate total Cost of Repairs per Sales Person Department.
+-- 20. Calculate total Cost of Repairs per Sales Person Department.
+Select top 2*
+	From PC_sales
+Select Sales_Person_Department, SUM(CAST(Cost_of_Repairs AS Int)) As Total_Cost_repairs
+	From PC_sales
+	Group by Sales_Person_Department
+
+
+-- ADVANCED QUESTIONS
+
+-- 21. Calculate profit per Shop Name.
+
+Select Shop_Name, SUM(Sale_Price-Discount_Amount) as Profit_Per_Shop_Name
+	From PC_sales
+	Group by Shop_Name
+
+-- 22. Calculate profit margin per sale ((Sale Price - Cost Price) / Sale Price).
+Select SUM(Sale_Price - Cost_Price / Sale_Price) as Profit_Margin_Per_Sale
+	From PC_sales
+-- 23. Determine which Continent has the highest total revenue.
+
+Select distinct top 1 Continent, MAX(Sale_Price-Discount_Amount) as Continent_with_Highest_Revenue
+	From PC_sales
+	Group by Continent
+
+-- 24. Calculate average Sale Price per RAM size.
+
+Select RAM, AVG(Sale_Price) as Avg_sale_per_ramsize
+	From PC_sales
+	Group by RAM
+	Order by Avg_sale_per_ramsize 
+
+-- 25. Find the PC Model with the highest Sale Price.
+
+Select Distinct Top 100 PC_Model, MAX(Sale_Price) as PC_model_with_max_saleprice
+	From PC_sales
+	Group by PC_Model
+	Order by PC_model_with_max_saleprice desc
+
+-- 26. Calculate the average number of days between Purchase Date and Ship Date.
+
+Select Sum(Convert(Purchase_date - Ship_Date) Date)
+	From PC_sales
+
+DATEDIFF(day, MIN(Purchase_Date), max(Ship_Date)) / nullif(Total-1,0) as Avg_Days
+	From PC_sales
+
+-- 27. Determine which Sales Person Department generates the highest revenue.
+
+Select Top 1 Sales_Person_Department, MAX(Sale_Price) as Highest_Rev_generation
+	From PC_sales
+	Group by Sales_Person_Department
+	Order by Highest_Rev_generation desc
+	
+-- 28. Calculate total revenue per Storage Capacity.
+
+Select Storage_Capacity, SUM(Sale_Price-Discount_Amount) as Total_revenue_Storage
+	From PC_sales
+	Group by Storage_Capacity
+-- 29. Identify sales where Sale Price is lower than PC Market Price.
+
+Select 
+-- 30. Rank Sales Person Name by Total Sales per Employee using a window function.
 
 -- ADVANCED QUESTIONS
 
